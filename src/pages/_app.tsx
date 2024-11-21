8a36cf9eee8095503ecad9c73a0f63d5826a7650
David Amunga <david.onyimbo@gmail.com>
2024-11-21 20:48:26 +0300

    feat: added plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+    {process.env.NODE_ENV === "production" && (
+           <Script
+           defer
+           data-domain="cucopy.com"
+           src="https://analytics.davidamunga.com/js/script.js"
+         />
+          )}
       <Component {...pageProps} />
       <Toaster position="top-right" />
     </>
